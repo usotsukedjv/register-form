5cd30cced9c92cf9c78dada8403dbceae2d580ca
usotsuke <you@example.com>
2025-08-02 15:29:26 +0300

    token in cookie add

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 require('dotenv').config()
+const cookieParser = require('cookie-parser')
 const jwt = require("jsonwebtoken");
 const bcrypt = require('bcrypt')
 const path = require('path')
 const {Pool} = require('pg')
 const app = express()
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public'))) 
@@ -20,38 +22,57 @@ app.get('/register', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public/registerForm.html'))
 })
 app.post('/registerSubmit', async (req, res)=>{
-   const {name, password} = req.body
-   const hash = await bcrypt.hash(password, 12)
-   const result = await pool.query("INSERT INTO users (name, password_hash) VALUES ($1, $2) RETURNING *", [name, hash])
-   const user = result.rows[0]
-   console.log(user)
-   res.json(user)
+   try {
+        const {name, password} = req.body
+        const hash = await bcrypt.hash(password, 12)
+        const result = await pool.query("INSERT INTO users (name, password_hash) VALUES ($1, $2) RETURNING id, name", [name, hash])
+        const {rows} = result
+        res.json(rows[0])
+   } catch (error) {
+    console.log(error)
+   }
 })
 app.get('/login', (req, res)=>{
     res.sendFile(path.join(__dirname, 'public/loginForm.html'))
 })
 app.post('/loginSubmit', async (req, res)=>{
   try {
-     const {name, password} = req.body
-     const {rows} = await pool.query("SELECT id, password_hash FROM users WHERE name=$1", [name])
-     const {id, password_hash} = rows[0]
-     if(!password_hash) throw new Error('user not found')
-     const match = await bcrypt.compare(password, password_hash)
+    const {name, password} = req.body
+    if(!name) {throw new Error('no name')}
+    if(!password) {throw new Error('no password')}
+    const result = await pool.query("SELECT * FROM users WHERE name=$1", [name])
+    const {rows} = result
+    const { id, password_hash} = rows[0]
+    const match = await bcrypt.compare(password, password_hash)
     if(!match) throw new Error('Wrong password')
     const token = jwt.sign({id}, process.env.JWT_SECRET)
-
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 
+    })
+    res.json({ message: "Login successful" });
   } catch (error) {
-    console.log(error)
-
+    return res.send(error)
   }
 })
 
 app.get('/profile', requireAuth, async (req, res)=>{
-
+    const result = await pool.query("SELECT name FROM users WHERE id=$1",[req.userId])
+    const {rows} = result
+    const {name} = rows[0]
+    res.send(`Hello ${name}, here is your profile`)
 })
 
 function requireAuth  (req, res, next)  {
-  
+//    const authHeader = req.headers.authorization
+//     const hToken = authHeader.split(" ")[1]
+    const cToken = req.cookies.token
+    const payload = jwt.verify(cToken, process.env.JWT_SECRET,  { expiresIn: "1h" })
+    console.log(payload)
+    if(!payload) return res.status(403).send('Invalid token')
+    req.userId = payload.id
+    next()
 };
 
 app.listen(process.env.PORT || 3000)
